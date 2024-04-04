44d979850b8d50b33d009438037c21c7deec4886
Maikoncarlos <maikonsaturno@gmail.com>
2024-04-03 21:00:44 -0300

    inserindo o CommandRunner na Classe de Application e chamada da classe

--- src/main/java/github/maikoncarlos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/github/maikoncarlos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,27 @@
 package github.maikoncarlos.screenmatch;
 
+import github.maikoncarlos.screenmatch.model.DadosSerie;
+import github.maikoncarlos.screenmatch.service.ConsumoApi;
+import github.maikoncarlos.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		var converteDados = new ConverteDados();
+		var dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
+	}
 }
